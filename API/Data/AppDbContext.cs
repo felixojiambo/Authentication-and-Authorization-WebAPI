653aff97c458b9e470f12f3b64fd2796a4009bfe
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-14 09:56:21 -0700

    using primary constructor in app db

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -2,12 +2,9 @@ using API.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data 
+namespace API.Data
 {
-    public class AppDbContext:IdentityDbContext<AppUser>{
-        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
-        {
-            
-        }
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
+    {
     }
 }
