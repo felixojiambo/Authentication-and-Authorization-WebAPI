40f20c58d788640fba2e5fad5ec30353b6529b84
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-14 23:24:51 -0700

    generate tokens

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -91,3 +91,47 @@ namespace API.Controllers
 
 
         }
+
+
+        private string GenerateToken(AppUser user){
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            var key = Encoding.ASCII
+            .GetBytes(_configuration.GetSection("JWTSetting").GetSection("securityKey").Value!);
+
+            var roles = _userManager.GetRolesAsync(user).Result;
+
+            List<Claim> claims = 
+            [
+                new (JwtRegisteredClaimNames.Email,user.Email??""),
+                new (JwtRegisteredClaimNames.Name,user.FullName??""),
+                new (JwtRegisteredClaimNames.NameId,user.Id ??""),
+                new (JwtRegisteredClaimNames.Aud,
+                _configuration.GetSection("JWTSetting").GetSection("validAudience").Value!),
+                new (JwtRegisteredClaimNames.Iss,_configuration.GetSection("JWTSetting").GetSection("validIssuer").Value!)
+            ];
+
+
+            foreach(var role in roles)
+
+            {
+                claims.Add(new Claim(ClaimTypes.Role,role));
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256
+                )
+            };
+
+
+            var token  = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+           
+
+        }
